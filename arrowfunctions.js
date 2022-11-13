cd28bb0d9ab61f8d82dfd236f686df6e03f713c6
femipraize <felixogbangwor@gmail.com>
2022-11-13 18:38:23 +0100

    ArrowFunctions

---
+++ arrowfunctions.js
@@ -0,0 +1,25 @@
+function func1(a, b){
+    return a+b;
+}
+
+var arrow = (a,b) => {
+    return a+b;
+}
+
+var arrow2 = (a,b) => a+b;
+
+ function func2() {
+    console.log("Wecome to auto x")
+}
+
+arrow2()
+
+var arrow3 = ()=>{
+    console.log(" Hello and welcome to Auto X")
+}
+arrow3()
+var arrow4 = name => {
+    console.log(" Hello!" + name+ "welcome to auto x")
+}
+
+arrow4("Femi")
