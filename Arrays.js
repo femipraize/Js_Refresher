be96c4a5b5f3e77c40c0e168af91ebcf8f4f0361
femipraize <felixogbangwor@gmail.com>
2022-11-13 18:36:14 +0100

    Arrays

---
+++ Arrays.js
@@ -0,0 +1,43 @@
+/*var car = ["Honda", "Toyota", "Hyundai", "Mercedes", "Jeep", "Buggati"]
+for (var i = 0; i<7; i++)
+{
+    document.write("<h1>"+car[i]+"</h1>")
+}
+------------------------------------------------
+
+var fruits = new Array("Orange", "banana", " mango")
+for (var i =0; i<3; i++)
+{
+    document.write("<h1>"+fruits[i]+"</h1>")
+}
+*/
+
+/* to add an element to an Array: Push method 
+
+var car = ["Honda", "Toyota", "Hyundai", "Mercedes", "Jeep", "Buggati"]
+car.push("Mistibushi")
+for (var i = 0; i<7; i++)
+{
+    document.write("<h1>"+car[i]+"</h1>")
+}
+
+*/
+
+/*
+var car = ["Honda", "Toyota", "Hyundai", "Mercedes", "Jeep", "Buggati"]
+car.pop("Buggati")
+car.push(500)
+for (var i = 0; i<car.length; i++)
+{
+    document.write("<h1>"+car[i]+"</h1>")
+}
+*/
+
+
+
+// whenever you use an function 
+// inside a class or an object it is called method
+
+
+
+
