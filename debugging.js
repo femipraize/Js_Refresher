77b9a7239ec1c7b4e9755ee6a3a99006d86c9eca
femipraize <felixogbangwor@gmail.com>
2022-11-15 19:40:19 +0100

    debugging

---
+++ debugging.js
@@ -0,0 +1,7 @@
+function add() 
+{
+    var no1 = parseInt(document.getElementById("n1").value);
+    var no2 = parseInt(document.getElementById("n2").value);
+    var no3 = no1 + no2;
+    alert("The sum is " + no3);   
+}
