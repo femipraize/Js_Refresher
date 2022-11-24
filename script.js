f48a5a37dcf64c4c7c62918cec4a11a8085d20d5
femipraize <felixogbangwor@gmail.com>
2022-11-24 18:01:21 +0100

    flappybird updated

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var hole = document.getElementById("hole");
 var game = document.getElementById("game");
+var result = document.getElementById("result");
+var text = document.getElementById("text");
+var score = 0;
+var jumping = 0;
 
 hole.addEventListener("animationiteration", RanHole)
 
-function Ranhole() {
-    var random = -((math.random()*350)+150)
+function RanHole() 
+{
+    var random = -((Math.random()*350)+150)
     hole.style.top = random+"px";
 }
+
+var fall = setInterval(function() 
+{
+    var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+    if(jumping == 0)
+    {
+        bird.style.top = (birdTop+2)+"px";
+    }
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    
+},10)
