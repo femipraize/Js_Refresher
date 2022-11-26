bf57e0cc827b34761856ce718460b036f8175f6f
femipraize <felixogbangwor@gmail.com>
2022-11-26 15:16:27 +0100

    script.js

--- script.js
+++ script.js
@@ -21,5 +21,26 @@ var fall = setInterval(function()
         bird.style.top = (birdTop+2)+"px";
     }
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    
-},10)
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var hTop = (500+holeTop)
+    if((birdTop > 450) || (blockLeft < 50)&& (blockLeft > -50)&& ((birdTop < hTop) || (birdTop > hTop + 100)))
+    {
+        result.style.display ="block";
+        text.innerText = `Your final score is ${score}`;
+        game.style.display = "none";
+        score = 0;
+    }
+}, 10)
+window.addEventListener("keydown", hop)
+function hop()
+{
+    jumping = 1;
+    var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+    if (birdTop > 6)
+    {
+        bird.style.top = (birdTop - 60) + "px"
+    }
+    setTimeout(function(){
+        jumping = 0;
+    },100)
+}
