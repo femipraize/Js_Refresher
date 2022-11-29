5599b0aee9da238443d0ea7b65ce6f6c6d16bb24
femipraize <felixogbangwor@gmail.com>
2022-11-29 15:15:45 +0100

    Projects

---
+++ Projects/app.js
@@ -0,0 +1,15 @@
+const cart = document.querySelector("nav .cart")
+const cartSidebar = document.querySelector(".cart-sidebar")
+const closeCart = document.querySelector(".close-cart")
+const burger = document.querySelector(".burger")
+const menuSidebar = document.querySelector("menu-sidebar")
+const closeMenu = document.querySelector(".close-menu")
+const cartItemsTotal = document.querySelector("noi")
+const cartPriceTotal = document.querySelector(".total-amount")
+const cartUi = document.querySelector(".cart-sidebar .cart")
+const totalDiv = document.querySelector(".total-sum")
+const clearBin = document.querySelector(".clear-cart-btn")
+const cartContent = document.querySelector(".cart-content")
+
+let Cart=[];
+let buttonsDom = [];
